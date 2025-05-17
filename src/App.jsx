422f5c5a8e00c79fc4350ea3c6c39f0accb9c43c
NethalaM <Mohan.Nethala@cevalogistics.com>
2025-05-17 21:07:59 +0530

    smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import CarsPage from "./pages/CarsPage";
 import About from "./pages/About";
 import Cardetails from "./pages/Cardetails";
 import Form from "./pages/Form";
 
+// ScrollToTop logic inside App
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
