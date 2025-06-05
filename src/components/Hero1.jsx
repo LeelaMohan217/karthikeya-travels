3c4be70337fe01b4512435ef53f8051368adbee9
NethalaM <Mohan.Nethala@cevalogistics.com>
2025-06-05 19:25:04 +0530

    zssdf

--- src/components/Hero1.jsx
+++ src/components/Hero1.jsx
@@ -6,33 +6,33 @@ import { MapPin } from "lucide-react";
 const Hero1 = () => {
   return (
     <section className="w-full min-h-screen bg-white flex items-center justify-center">
-      <div className="max-w-7xl mx-auto py-12 px-6 md:px-0">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
-          <div className="flex flex-col item-start justify-center space-y-6">
-            <div className="flex flex-col justify-center items-start space-y-6">
+      <div className="max-w-5xl mx-auto py-8 md:py-12 px-6 md:px-0">
+        <div className="grid grid-cols-1 gap-8 items-center">
+          <div className="flex flex-col item-center justify-center space-y-6">
+            <div className="flex flex-col justify-center items-center space-y-6">
               <div className="p-[1px] rounded-full bg-gradient-to-r from-orange-500 via-red-600 to-yellow-400 mb-2">
                 <div className="flex justify-center items-baseline gap-2 rounded-full py-2 px-4 bg-white">
-                  <p className="text-orange-600 uppercase tracking-wide text-sm font-semibold text-left">
+                  <p className="text-orange-600 uppercase tracking-wide text-xs font-semibold">
                     Plan Your Destination
                   </p>
                   <MapPin className="w-4 h-4 text-orange-600" />
                 </div>
               </div>
-              <h2 className="text-5xl md:text-6xl font-bold  text-orange-600 leading-14 md:leading-16">
-                Save big with <br />{" "}
+              <h2 className="text-3xl md:text-6xl font-bold  text-orange-600 text-center">
+                Save big with <br /> <span>Sri</span> {" "}
                 <span className="bg-gradient-to-r from-orange-500 via-red-600 to-yellow-400 bg-clip-text text-transparent">
-                  Sri Karthikeya
+                  Karthikeya
                 </span>{" "}
                 car rentals
               </h2>
-              <p className="text-base md:text-lg text-orange-800">
+              <p className="text-base md:text-lg text-orange-800 text-center">
                 Enjoy premium cars, 24/7 support, flexible plans, and seamless
                 customer service. Our glass-clear pricing and luxury experience
                 set us apart.
               </p>
             </div>
 
-            <div className="flex sm:flex-row flex-col item-start gap-6">
+            <div className="flex sm:flex-row flex-col justify-center item-center gap-6">
               <Link
                 to="/about"
                 className="px-5 py-3 text-base border text-center border-orange-600 text-orange-600 rounded-md hover:bg-orange-200 transition-all duration-300 font-medium"
@@ -47,7 +47,7 @@ const Hero1 = () => {
               </Link>
             </div>
           </div>
-          <div className="hidden md:block">
+          <div className="block md:hidden">
             <img
               src={hero}
               alt="Car"
