5fc8685dad9c02772232172a8c3b0247da15d82e
NethalaM <Mohan.Nethala@cevalogistics.com>
2025-06-05 16:21:54 +0530

    hero page modifications

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <section className="relative w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-orange-50 to-orange-100 overflow-hidden">
       <div className="max-w-5xl mx-auto z-10 px-6 py-12 text-gray-800 ">
-        <p className="text-orange-600 uppercase tracking-wide text-sm md:text-base font-semibold text-left md:text-center">
+        <p className="text-orange-600 uppercase tracking-wide text-base md:text-lg font-semibold text-left md:text-center">
           Plan Your Destination
         </p>
-        <h2 className="text-5xl md:text-5xl font-bold mb-4 text-orange-800 text-left md:text-center">
+        <h2 className="text-6xl md:text-7xl font-bold mb-4 text-orange-700 text-left md:text-center">
           Save big with Sri Karthikeya car rentals
         </h2>
         <p className="text-sm md:text-lg text-orange-800 mb-6 text-left md:text-center">
@@ -20,22 +20,22 @@ const Hero = () => {
         <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6 text-center">
           <Link
             to="/about"
-            className="px-5 py-3 border border-orange-600 text-gray-800 rounded-md hover:bg-orange-200 transition-all duration-300 font-medium"
+            className="px-5 py-3 text-base border border-orange-600 text-orange-800 rounded-md hover:bg-orange-200 transition-all duration-300 font-medium"
           >
             Learn More
           </Link>
           <Link
             to="/form"
-            className="px-5 py-3 rounded-md bg-orange-600 text-white hover:bg-orange-700 transition-all duration-300 font-medium"
+            className="px-5 py-3 text-base rounded-md bg-orange-600 text-white hover:bg-orange-700 transition-all duration-300 font-medium"
           >
             Book a ride
           </Link>
         </div>
       </div>
 
-      <div className="w-96 h-96 bg-orange-500 opacity-30 blur-3xl rounded-full absolute -top-20 -left-20"></div>
+      {/* <div className="w-96 h-96 bg-orange-500 opacity-30 blur-3xl rounded-full absolute -top-20 -left-20"></div>
       <div className="w-96 h-96 bg-orange-500 opacity-30 blur-3xl rounded-full absolute -bottom-20 -right-20"></div>
-      <div className="w-56 h-56 bg-orange-500 opacity-30 blur-3xl rounded-full absolute bottom-40 left-100"></div>
+      <div className="w-56 h-56 bg-orange-500 opacity-30 blur-3xl rounded-full absolute bottom-40 left-100"></div> */}
     </section>
   );
 };
