3255c1489a4a90d8d4a457164ce6f339df0a5fe7
NethalaM <Mohan.Nethala@cevalogistics.com>
2025-06-05 16:13:45 +0530

    mobile fisrt check

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <section className="relative w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-orange-50 to-orange-100 overflow-hidden">
-      <div className="max-w-5xl mx-auto z-10 px-6 py-12 text-gray-800 text-center">
-        <p className="text-orange-600 uppercase tracking-wide text-sm font-semibold">
+      <div className="max-w-5xl mx-auto z-10 px-6 py-12 text-gray-800 ">
+        <p className="text-orange-600 uppercase tracking-wide text-sm md:text-base font-semibold text-left md:text-center">
           Plan Your Destination
         </p>
-        <h2 className="text-3xl md:text-5xl font-bold mb-4 text-orange-900">
+        <h2 className="text-5xl md:text-5xl font-bold mb-4 text-orange-800 text-left md:text-center">
           Save big with Sri Karthikeya car rentals
         </h2>
-        <p className="text-md md:text-lg text-orange-800 mb-6">
+        <p className="text-sm md:text-lg text-orange-800 mb-6 text-left md:text-center">
           Enjoy premium cars, 24/7 support, flexible plans, and seamless
           customer service. Our glass-clear pricing and luxury experience set us
           apart.
         </p>
 
-        <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6">
+        <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6 text-center">
           <Link
             to="/about"
             className="px-5 py-3 border border-orange-600 text-gray-800 rounded-md hover:bg-orange-200 transition-all duration-300 font-medium"
@@ -33,9 +33,9 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="w-96 h-96 bg-orange-300 opacity-30 blur-3xl rounded-full absolute -top-20 -left-20"></div>
-      <div className="w-96 h-96 bg-orange-300 opacity-30 blur-3xl rounded-full absolute -bottom-20 -right-20"></div>
-      <div className="w-56 h-56 bg-orange-300 opacity-30 blur-3xl rounded-full absolute bottom-40 left-100"></div>
+      <div className="w-96 h-96 bg-orange-500 opacity-30 blur-3xl rounded-full absolute -top-20 -left-20"></div>
+      <div className="w-96 h-96 bg-orange-500 opacity-30 blur-3xl rounded-full absolute -bottom-20 -right-20"></div>
+      <div className="w-56 h-56 bg-orange-500 opacity-30 blur-3xl rounded-full absolute bottom-40 left-100"></div>
     </section>
   );
 };
