98e576d336cbcace0e3a613e6f2da54bf47119b3
NethalaM <ext.mohan.nethala@cevalogistics.com>
2025-06-14 18:51:13 +0530

    removed mileage

--- src/components/car/IndividualCarDetails.jsx
+++ src/components/car/IndividualCarDetails.jsx
@@ -31,7 +31,7 @@ const IndividualCarDetails = ({ car }) => {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 my-6 text-base md:text-lg text-gray-800 text-center ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-6 text-base md:text-lg text-gray-800 text-center ">
           <div className="p-4 flex justify-center items-center gap-2 md:border-r-2">
             <FuelIcon /> Fuel Type:{" "}
             <span className="font-medium">{car.fuelType}</span>
@@ -39,10 +39,6 @@ const IndividualCarDetails = ({ car }) => {
           <div className="p-4 flex justify-center items-center gap-2 md:border-r-2">
             <CarFrontIcon /> Seating:{" "}
             <span className="font-medium">{car.seatingCapacity}</span>
-          </div>
-          <div className="p-4 flex justify-center items-center gap-2 md:border-r-2">
-            <CarTaxiFrontIcon /> Mileage:{" "}
-            <span className="font-medium">{car.mileage}</span>
           </div>
           <div className="p-4 flex justify-center items-center gap-2">
             <PersonStanding /> Drive:{" "}
